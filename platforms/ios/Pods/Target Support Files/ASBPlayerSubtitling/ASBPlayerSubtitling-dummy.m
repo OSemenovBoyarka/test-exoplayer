7a7e2a278e6c06ac8d8383f97f89a70336c4bdc6
Alexander Semenov <alexs@2kgroup.com>
2017-07-08 19:15:56 +0300

    Initial commit

---
+++ platforms/ios/Pods/Target Support Files/ASBPlayerSubtitling/ASBPlayerSubtitling-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ASBPlayerSubtitling : NSObject
+@end
+@implementation PodsDummy_ASBPlayerSubtitling
+@end
---
+++ platforms/ios/build/emulator/testexoplayer.app/app/tns_modules/nativescript-exoplayer/videoplayer-common.ts
@@ -0,0 +1,134 @@
+﻿import videoSource = require("./video-source/video-source");
+import subtitleSource = require("./subtitle-source/subtitle-source");
+import * as definitions from "./index";
+import { isFileOrResourcePath } from "utils/utils"
+import { isString } from "utils/types"
+import { View, Property, booleanConverter } from "ui/core/view";
+
+// on Android we explicitly set propertySettings to None because android will invalidate its layout (skip unnecessary native call).
+// var AffectsLayout = platform.device.os === platform.platformNames.android ? dependencyObservable.PropertyMetadataSettings.None : dependencyObservable.PropertyMetadataSettings.AffectsLayout;
+
+function onSrcPropertyChanged(view, oldValue, newValue) {
+
+    const video = view;
+    let value = newValue;
+
+    if (isString(value)) {
+        value = value.trim();
+        video.videoSource = null;
+        video["_url"] = value;
+        video.isLoadingProperty = true;
+        if (isFileOrResourcePath(value)) {
+            video.videoSource = videoSource.fromFileOrResource(value);
+            video.isLoadingProperty = false;
+        } else {
+            if (video["_url"] === value) {
+                video.videoSource = videoSource.fromUrl(value);
+                video.isLoadingProperty = false;
+            }
+        }
+    } else if (value instanceof videoSource.VideoSource) {
+        video.videoSource = value;
+    } else {
+        video.videoSource = videoSource.fromNativeSource(value);
+    }
+}
+
+function onSubtitlesPropertyChanged(view, oldValue, newValue) {
+    const video = view;
+    if (isString(newValue)) {
+        let value = newValue.trim()
+        video.subtitleSource = null
+        if (isFileOrResourcePath(value)){
+            video.subtitleSource = subtitleSource.fromFileOrResource(value);
+        } else {
+            video.subtitleSource = subtitleSource.fromUrl(value);
+        }
+    }
+}
+
+
+export class Video extends View {
+    public static finishedEvent: string = "finished";
+    public static playbackReadyEvent: string = "playbackReady";
+    public static playbackStartEvent: string = "playbackStart";
+    public static seekToTimeCompleteEvent: string = "seekToTimeComplete";
+    public static currentTimeUpdatedEvent: string = "currentTimeUpdated";
+
+    public _emit: any;
+    public android: any;
+    public ios: any;
+    public src: string; /// video source file
+    public subtitles: string; /// subtitles source file
+    public subtitleSource: string; /// subtitle source content
+    public observeCurrentTime: boolean; // set to true if want to observe current time.
+    public autoplay: boolean = false; /// set true for the video to start playing when ready
+    public controls: boolean = true; /// set true to enable the media player's playback controls
+    public loop: boolean = false; /// whether the video loops the playback after extends
+    public muted: boolean = false;
+    public fill: boolean = false;
+}
+
+export const srcProperty = new Property<Video, any>({
+    name: "src",
+    valueChanged: onSrcPropertyChanged
+});
+srcProperty.register(Video);
+
+export const subtitlesProperty = new Property<Video, any>({
+    name: "subtitles",
+    valueChanged: onSubtitlesPropertyChanged
+});
+subtitlesProperty.register(Video);
+
+export const subtitleSourceProperty = new Property<Video, any>({
+    name: "subtitleSource",
+});
+subtitleSourceProperty.register(Video);
+
+export const videoSourceProperty = new Property<Video, any>({
+    name: "videoSource",
+});
+videoSourceProperty.register(Video);
+
+export const isLoadingProperty = new Property<Video, boolean>({
+    name: "isLoading",
+    valueConverter: booleanConverter,
+});
+isLoadingProperty.register(Video);
+
+export const observeCurrentTimeProperty = new Property<Video, boolean>({
+    name: "observeCurrentTime",
+    valueConverter: booleanConverter,
+});
+observeCurrentTimeProperty.register(Video);
+
+export const autoplayProperty = new Property<Video, boolean>({
+    name: "autoplay",
+    valueConverter: booleanConverter,
+});
+autoplayProperty.register(Video);
+
+export const controlsProperty = new Property<Video, boolean>({
+    name: "controls",
+    valueConverter: booleanConverter,
+});
+controlsProperty.register(Video);
+
+export const loopProperty = new Property<Video, boolean>({
+    name: "loop",
+    valueConverter: booleanConverter,
+});
+loopProperty.register(Video);
+
+export const mutedProperty = new Property<Video, boolean>({
+    name: "muted",
+    valueConverter: booleanConverter,
+});
+mutedProperty.register(Video);
+
+export const fillProperty = new Property<Video, boolean>({
+    name: "fill",
+    valueConverter: booleanConverter,
+});
+fillProperty.register(Video);
