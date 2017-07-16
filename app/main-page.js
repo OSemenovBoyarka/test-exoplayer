d9626ecd6f3c8e3b2530643d1959be037ced0680
Alexander Semenov <alexs@2kgroup.com>
2017-07-16 13:40:56 +0300

    Updated demo to show subtitle change controls

--- app/main-page.js
+++ app/main-page.js
@@ -1,41 +1,17 @@
-/*
-In NativeScript, a file with the same name as an XML file is known as
-a code-behind file. The code-behind is a great place to place your view
-logic, and to set up your page’s data binding.
-*/
-
-/*
-NativeScript adheres to the CommonJS specification for dealing with
-JavaScript modules. The CommonJS require() function is how you import
-JavaScript modules defined in other files.
-*/ 
-var createViewModel = require("./main-view-model").createViewModel;
-
-function onNavigatingTo(args) {
-    /*
-    This gets a reference this page’s <Page> UI component. You can
-    view the API reference of the Page to see what’s available at
-    https://docs.nativescript.org/api-reference/classes/_ui_page_.page.html
-    */
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var main_view_model_1 = require("./main-view-model");
+var platform_1 = require("platform");
+var color_1 = require("color");
+var application_1 = require("application");
+// Event handler for Page "loaded" event attached in main-page.xml
+function pageLoaded(args) {
+    // Get the event sender
     var page = args.object;
-
-    /*
-    A page’s bindingContext is an object that should be used to perform
-    data binding between XML markup and JavaScript code. Properties
-    on the bindingContext can be accessed using the {{ }} syntax in XML.
-    In this example, the {{ message }} and {{ onTap }} bindings are resolved
-    against the object returned by createViewModel().
-
-    You can learn more about data binding in NativeScript at
-    https://docs.nativescript.org/core-concepts/data-binding.
-    */
-    page.bindingContext = createViewModel();
+    page.bindingContext = new main_view_model_1.HelloWorldModel(page);
+    if (platform_1.isAndroid && platform_1.device.sdkVersion >= "21") {
+        var window_1 = application_1.android.startActivity.getWindow();
+        window_1.setStatusBarColor(new color_1.Color("#d32f2f").android);
+    }
 }
-
-/*
-Exporting a function in a NativeScript code-behind file makes it accessible
-to the file’s corresponding XML file. In this case, exporting the onNavigatingTo
-function here makes the navigatingTo="onNavigatingTo" binding in this page’s XML
-file work.
-*/
-exports.onNavigatingTo = onNavigatingTo;
+exports.pageLoaded = pageLoaded;
--- app/main-view-model.js
+++ app/main-view-model.js
@@ -1,24 +1,161 @@
-var Observable = require("data/observable").Observable;
-
-function getMessage(counter) {
-    if (counter <= 0) {
-        return "Hoorraaay! You unlocked the NativeScript clicker achievement!";
-    } else {
-        return counter + " taps left";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var observable_1 = require("data/observable");
+var platform_1 = require("platform");
+var timer_1 = require("timer");
+var HelloWorldModel = (function (_super) {
+    __extends(HelloWorldModel, _super);
+    function HelloWorldModel(mainpage) {
+        var _this = _super.call(this) || this;
+        _this.completed = false;
+        _this._videoPlayer = mainpage.getViewById("nativeVideoPlayer");
+        _this.currentTime = "";
+        _this.videoDuration = "";
+        _this.videoSrc = "~/videos/big_buck_bunny.mp4";
+        _this.subtitlesSrc = "~/videos/sample.srt";
+        _this.trackVideoCurrentPosition();
+        return _this;
     }
-}
-
-function createViewModel() {
-    var viewModel = new Observable();
-    viewModel.counter = 42;
-    viewModel.message = getMessage(viewModel.counter);
-
-    viewModel.onTap = function() {
-        this.counter--;
-        this.set("message", getMessage(this.counter));
-    }
-
-    return viewModel;
-}
-
-exports.createViewModel = createViewModel;
+    HelloWorldModel.prototype.setEnglishSubtitles = function () {
+        this._videoPlayer.subtitles = "~/videos/sample.srt";
+    };
+    HelloWorldModel.prototype.setRussianSubtitles = function () {
+        this._videoPlayer.subtitles = "~/videos/sample-ru.srt";
+    };
+    HelloWorldModel.prototype.disableSubtitles = function () {
+        this._videoPlayer.subtitles = "";
+    };
+    /**
+     * Video Finished callback
+     */
+    HelloWorldModel.prototype.videoFinished = function (args) {
+        this.completed = true;
+    };
+    HelloWorldModel.prototype.playbackStart = function (args) {
+        this.completed = false;
+    };
+    /**
+     * Pause the video
+     */
+    HelloWorldModel.prototype.pauseVideo = function () {
+        this._videoPlayer.pause();
+    };
+    /**
+     * Play the video
+     */
+    HelloWorldModel.prototype.playVideo = function () {
+        this._videoPlayer.play();
+        this.completed = false;
+    };
+    /**
+     * Stop the video player
+     */
+    HelloWorldModel.prototype.stopVideo = function () {
+        if (platform_1.isAndroid) {
+            this._videoPlayer.stop();
+        }
+    };
+    /**
+     * Get the video duration
+     */
+    HelloWorldModel.prototype.getVideoDuration = function () {
+        var videoDuration = this._videoPlayer.getDuration();
+        console.log("Video Duration: " + videoDuration);
+        this.set("videoDuration", videoDuration);
+    };
+    /**
+     * Go to 30 seconds
+     */
+    HelloWorldModel.prototype.goToTime = function () {
+        try {
+            this._videoPlayer.seekToTime(30000);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    };
+    HelloWorldModel.prototype.animate = function () {
+        var _this = this;
+        console.log("Animation");
+        var enums = require("ui/enums");
+        this._videoPlayer.animate({
+            rotate: 360,
+            duration: 3000,
+            curve: enums.AnimationCurve.spring
+        }).then(function () {
+            return _this._videoPlayer.animate({
+                rotate: 0,
+                duration: 3000,
+                curve: enums.AnimationCurve.spring
+            });
+        }).then(function () {
+            return _this._videoPlayer.animate({
+                scale: { x: .5, y: .5 },
+                duration: 1000,
+                curve: enums.AnimationCurve.spring
+            });
+        }).then(function () {
+            return _this._videoPlayer.animate({
+                scale: { x: 1.5, y: 1.5 },
+                duration: 3000,
+                curve: enums.AnimationCurve.spring
+            });
+        }).then(function () {
+            return _this._videoPlayer.animate({
+                scale: { x: 1.0, y: 1.0 },
+                duration: 3000,
+                curve: enums.AnimationCurve.spring
+            });
+        });
+    };
+    HelloWorldModel.prototype.muteVideo = function () {
+        this._videoPlayer.mute(true);
+    };
+    HelloWorldModel.prototype.unmuteVideo = function () {
+        this._videoPlayer.mute(false);
+    };
+    /**
+     * Get the video current time
+     */
+    HelloWorldModel.prototype.getVideoCurrentTime = function () {
+        try {
+            var currentTime = this._videoPlayer.getCurrentTime();
+            console.log("Current Time: " + currentTime);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    };
+    /**
+     * Change the video src property
+     */
+    HelloWorldModel.prototype.changeVideoSource = function () {
+        //  this._videoPlayer.src = "~/videos/test_video_rotated.mp4";
+        //  return;
+        if (this.videoSrc === "~/videos/small.mp4") {
+            this._videoPlayer.src = "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4";
+        }
+        else {
+            this._videoPlayer.src = "~/videos/small.mp4";
+        }
+    };
+    HelloWorldModel.prototype.trackVideoCurrentPosition = function () {
+        var _this = this;
+        var trackInterval = timer_1.setInterval(function () {
+            var x, y;
+            if (_this.completed) {
+                x = "";
+                y = "";
+            }
+            else {
+                x = _this._videoPlayer.getCurrentTime();
+                y = _this._videoPlayer.getDuration();
+            }
+            _this.set("currentTime", x);
+            _this.set("videoDuration", y);
+        }, 200);
+        return trackInterval;
+    };
+    return HelloWorldModel;
+}(observable_1.Observable));
+exports.HelloWorldModel = HelloWorldModel;
